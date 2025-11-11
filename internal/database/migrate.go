53ae10049ffeb36f4533b53256cad9062f20fa90
mink0ff <minko.serezha@mail.ru>
2025-11-11 17:00:04 +0300

    поправил названия миграций и подключил их в main

---
+++ internal/database/migrate.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func RunMigrations(host, port, user, password, dbname string) {
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		user, password, host, port, dbname)
+
+	absPath, err := filepath.Abs("migrations")
+	fmt.Println(absPath)
+	if err != nil {
+		log.Fatalf("Failed to get absolute path for migrations: %v", err)
+	}
+
+	m, err := migrate.New("file://"+absPath, dsn)
+	if err != nil {
+		log.Fatalf("Failed to initialize migrations: %v", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatalf("Migration failed: %v", err)
+	}
+
+	log.Println("Migrations applied successfully.")
+}
