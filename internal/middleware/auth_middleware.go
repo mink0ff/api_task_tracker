4ab0a2cd251ff9d5a2ac4fc42bde4ebb3b8e7211
mink0ff <minko.serezha@mail.ru>
2025-11-25 20:27:08 +0300

    реализация auth_middleware

--- internal/middleware/auth_middleware.go
+++ internal/middleware/auth_middleware.go
@@ -1,1 +1,38 @@
 package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/mink0ff/api_task_tracker/internal/auth"
+)
+
+func JWTAuth(jwtManager *auth.JWTManager) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			authHeader := r.Header.Get("Authorization")
+			if authHeader == "" {
+				http.Error(w, "missing token", http.StatusUnauthorized)
+				return
+			}
+
+			parts := strings.Split(authHeader, " ")
+			if len(parts) != 2 || parts[0] != "Bearer" {
+				http.Error(w, "invalid token format", http.StatusUnauthorized)
+				return
+			}
+
+			claims, err := jwtManager.Verify(parts[1])
+			if err != nil {
+				http.Error(w, "invalid token", http.StatusUnauthorized)
+				return
+			}
+
+			ctx := r.Context()
+			ctx = auth.SetUserID(ctx, claims.UserID)
+
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
